e8f3894c026700c0fe97086e7536fc5666628fca
Danya Generalov <danya@danya02.ru>
2022-07-01 21:35:36 +0300

    Fixing issues with code so it runs correctly.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import json
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
+intents.members = True
 bot = commands.Bot(command_prefix='?', intents=intents)
 
 def get_config():
@@ -25,11 +26,12 @@ LAST_TIMER_UPDATE = 0
 async def check_status():
     global LAST_UPDATE
     global LAST_TIMER_UPDATE
+    global TIMER_MESSAGE
     config = get_config()
     if time.time() - LAST_UPDATE > config['update_interval']:
         TIMER_MESSAGE = await run_update(config)
         LAST_UPDATE = time.time()
-    if time.time() - LAST_TIMER_UPDATE > config['timer_interval']:
+    elif time.time() - LAST_TIMER_UPDATE > config['refresh_interval']:
         await update_timer(config, TIMER_MESSAGE)
         LAST_TIMER_UPDATE = time.time()
 
@@ -37,7 +39,7 @@ async def check_status():
 
 async def run_update(config):
     # Wipe the notification channel.
-    channel: discord.TextChannel = bot.get_channel(config['notification_channel'])
+    channel: discord.TextChannel = bot.get_channel(config['channel_id'])
     await channel.purge()
 
     guild: discord.Guild = channel.guild
@@ -57,20 +59,28 @@ async def run_update(config):
         await channel.send(config['text_post'])
 
     # Send the timer message
-    return await channel.send(config['timer_message'].format(f'{config["update_interval"]} seconds'))
+    return await channel.send(config['text_time_until_update'].format(f'{config["update_interval"]} seconds'))
 
 
 async def update_timer(config, message):
     try:
         remaining_time = config['update_interval'] - (time.time() - LAST_UPDATE)
-        hour, minute = divmod(remaining_time, 60)
+        hour, minute = divmod(remaining_time, 3600)
+        hour = int(hour)
+        minute = int(minute)
         minute, second = divmod(minute, 60)
+        minute = int(minute)
         second = int(second)
         if message is None:
             raise Exception
-        await message.edit(content=config['timer_message'].format(f'{hour}:{minute:02d}:{second:02d}'))
+        await message.edit(content=config['text_time_until_update'].format(f'{hour}:{minute:02d}:{second:02d}'))
     except Exception as e:
         logging.error(e)
         pass
+
+@bot.event
+async def on_ready():
+    logging.info(f'Logged in as {bot.user.name}')
+    check_status.start()
 
 bot.run(TOKEN)
